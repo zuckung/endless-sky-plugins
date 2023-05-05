053d3e2cc92bcd5db62c6ecca337adfbcf77e791
zuckung <zuckung@gmx.de>
2023-05-05 23:43:26 +0200

    Update zipfiles.py

--- src/zipfiles.py
+++ src/zipfiles.py
@@ -7,18 +7,18 @@ plugins = set()
 for f in changed.split("%%%"):
 	if not "myplugins" in f:
 		continue
-		path = f.split(os.sep)
-		index = path.index("myplugins") + 1
-		if index >= len(path):
+	path = f.split(os.sep)
+	index = path.index("myplugins") + 1
+	if index >= len(path):
    		continue
-		plugins.add(path[index])
-		if plugins:
-			print("The following plugins have changed:")
-			for p in plugins:
-				print(p)
-      os.chdir('myplugins/')
-      x = p.replace(" ", ".")
-      subprocess.run(["zip", "-r", "../" + x + ".zip", p], stdout=subprocess.DEVNULL)
-      os.chdir('../')
-   else:
-     print("No plugin changes have been detected.")
+	plugins.add(path[index])
+if plugins:
+	print("The following plugins have changed:")
+	for p in plugins:
+		print(p)
+		os.chdir('myplugins/')
+		x = p.replace(" ", ".")
+		subprocess.run(["zip", "-r", "../" + x + ".zip", p], stdout=subprocess.DEVNULL)
+		os.chdir('../')
+else:
+	print("No plugin changes have been detected.")
