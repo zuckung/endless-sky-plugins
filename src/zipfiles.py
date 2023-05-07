59d5004897d1406f8f76ee4283c104559bfad47b
zuckung <zuckung@gmx.de>
2023-05-07 22:34:38 +0200

    test

--- src/zipfiles.py
+++ src/zipfiles.py
@@ -8,24 +8,33 @@ pos = changed.index(".py', '") + 7
 changed = changed[pos:]
 changed = changed.replace("', '"," ")
 changed = changed.replace("']","")
-print(changed)
+print("arguments are : " + changed)
 
-plugins = set()
-for f in changed.split("%25%25%25"):
-    if not "myplugins" in f:
-        continue
-    path = f.split(os.sep)
-    index = path.index("myplugins") + 1
-    if index >= len(path):
-        continue
-    plugins.add(path[index])
-if plugins:
-    print("The following plugins have changed:")
-    for p in plugins:
-        print(p)
-        os.chdir('myplugins/')
-        x = p.replace(" ", ".")
-        subprocess.run(["zip", "-r", "../" + x + ".zip", p], stdout=subprocess.DEVNULL)
-        os.chdir('../')
+
+if changed = "-all":
+    names = os.listdir("myplugins/")
+    for name in names:
+            os.chdir('myplugins/')
+            x = p.replace(" ", ".")
+            subprocess.run(["zip", "-r", "../" + x + ".zip", p], stdout=subprocess.DEVNULL)
+            os.chdir('../')
 else:
-    print("No plugin changes have been detected.")
+    plugins = set()
+        for f in changed.split("%25%25%25"):
+        if not "myplugins" in f:
+            continue
+        path = f.split(os.sep)
+        index = path.index("myplugins") + 1
+        if index >= len(path):
+            continue
+        plugins.add(path[index])
+    if plugins:
+        print("The following plugins have changed:")
+        for p in plugins:
+            print(p)
+            os.chdir('myplugins/')
+            x = p.replace(" ", ".")
+            subprocess.run(["zip", "-r", "../" + x + ".zip", p], stdout=subprocess.DEVNULL)
+            os.chdir('../')
+    else:
+        print("No plugin changes have been detected.")
