1e74f35d4b5d6259eeda333a22ad012f4a696acc
zuckung <zuckung@gmx.de>
2024-08-05 16:56:31 +0200

    functionality change (finally working)

--- res/src/makemd.py
+++ res/src/makemd.py
@@ -4,31 +4,31 @@ import requests
 from datetime import datetime, date
 
 # for local testing
-if os.getcwd() == "/storage/emulated/0/Download/mgit/test/res/src": # check for local testing
-	os.chdir("../../")
+if os.getcwd() == '/storage/emulated/0/Download/mgit/test/res/src': # check for local testing
+	os.chdir('../../')
 
 # read paths and files
-with open("res/paths.txt") as f:
+with open('res/paths.txt') as f:
 	for line in f:
 		line = str((line.strip()))
-		if line.find("pathtoplugins") == 0:	# i.e. pathtoplugins = myplugins/
-			pathtoplugins = line.split(" = ")[1]
-		elif line.find("indexfile") == 0: 		# i.e.indexfile = README.md
-			indexfile = line.split(" = ")[1]
-		elif line.find("assetfiles") == 0:	   # i.e. assetfiles = https://github.com/zuckung/test/releases/download/Latest/
- 			assetfiles = line.split(" = ")[1]
-		elif line.find("pluginurl") == 0:		 # i.e pluginurl = https://github.com/zuckung/test/tree/main/myplugins/
-			pluginurl = line.split(" = ")[1]
-		elif line.find("template") == 0:			# i.e. template = res/template.txt
-			templatefile = line.split(" = ")[1]
-		elif line.find("picturefile") == 0:	  # i.e picturefile = res/icon.png
-			picturefile = line.split(" = ")[1]
+		if line.find('pathtoplugins') == 0:	# i.e. pathtoplugins = myplugins/
+			pathtoplugins = line.split(' = ')[1]
+		elif line.find('indexfile') == 0: 		# i.e.indexfile = README.md
+			indexfile = line.split(' = ')[1]
+		elif line.find('assetfiles') == 0:	   # i.e. assetfiles = https://github.com/zuckung/test/releases/download/Latest/
+ 			assetfiles = line.split(' = ')[1]
+		elif line.find('pluginurl') == 0:		 # i.e pluginurl = https://github.com/zuckung/test/tree/main/myplugins/
+			pluginurl = line.split(' = ')[1]
+		elif line.find('template') == 0:			# i.e. template = res/template.txt
+			templatefile = line.split(' = ')[1]
+		elif line.find('picturefile') == 0:	  # i.e picturefile = res/icon.png
+			picturefile = line.split(' = ')[1]
 
 # read templates
-with open(templatefile, "r") as file1:
+with open(templatefile, 'r') as file1:
 	template = file1.read()
-header = template.split("%plugin template below this line%")[0]
-p_template =  template.split("%plugin template below this line%")[1]
+header = template.split('%plugin template below this line%')[0]
+p_template =  template.split('%plugin template below this line%')[1]
 
 # reading news.txt
 with open('res/news.txt') as file1:
@@ -65,7 +65,7 @@ for entry in entries:
 		if index == column +1 or index == 2*column +2:
 			pluginlist += '<img width="294" height="1"><br></td><td><img width="294" height="1"><br>\n'
 pluginlist += '<img width="294" height="1"><br></td></tr></table>\n'
-with open(indexfile, "w") as file1:
+with open(indexfile, 'w') as file1:
 	file1.writelines(header.replace('%pluginlist%', pluginlist).replace('%news%', news))
 
 
@@ -73,37 +73,54 @@ with open(indexfile, "w") as file1:
 entries = os.listdir(pathtoplugins)
 entries = sorted(entries)
 for entry in entries:
-	withdots = entry.replace(" ", ".")
-	forweb  = entry.replace(" ", "%20")
+	withdots = entry.replace(' ', '.')
+	forweb  = entry.replace(' ', '%20')
 	pa_template = p_template
-
+	
+	# get version number
+	with open('res/versioning.txt', 'r') as read_version:
+		version_lines = read_version.readlines()
+	found = 0
+	for vline in version_lines:
+		split = vline.split('|')
+		if split[0] == withdots:
+			version_number = split[1].replace('\n', '')
+			found = 1
+			break
+	if found == 0:
+		version_number = '1.0.0'
+	assetfiles = 'https://github.com/zuckung/endless-sky-plugins/releases/download/v' + version_number + '-' + withdots + '/'
+	
 	# get description out of about.txt
-	with open(pathtoplugins + entry + "/about.txt" , "r") as file1:
+	with open(pathtoplugins + entry + '/about.txt' , 'r') as file1:
 		description_list = file1.readlines()
-	description = ""
+	description = ''
 	for line in description_list:
-		description = description + ">" + line
+		description = description + '>' + line
 		
 	# get readme.md
-	with open(pathtoplugins + entry + "/README.md" , "r") as file1:
+	with open(pathtoplugins + entry + '/README.md' , 'r') as file1:
 		readme_list = file1.readlines()
-	readme = ""
+	readme = ''
 	for line in readme_list:
-		readme = readme + line + "\n"
+		readme = readme + line + '\n'
           
 	# get last modified date from the assetfiles
 	try:
-		response = requests.head(assetfiles + withdots + ".zip", allow_redirects=True)
+		response = requests.head(assetfiles + withdots + '.zip', allow_redirects=True)
+		if response.status_code == 404:
+			assetfiles = assetfiles.replace('1.0.0', '1.0')
+			response = requests.head(assetfiles + withdots + '.zip', allow_redirects=True)
 		modif = response.headers['Last-Modified']
 		datetime_object = datetime.strptime(modif, '%a, %d %b %Y %H:%M:%S %Z')
 		modif = str(datetime_object.date())
 
 	# get file size of the assetfiles in kb or mb
 		assetsize = int(response.headers['Content-Length']) / 1024
-		form = " kb"
+		form = ' kb'
 		if assetsize > 1024 :
 			assetsize = assetsize / 1024
-			form = " mb"
+			form = ' mb'
 	except:
 		modif = 'N/A'
 		form = ''
@@ -117,32 +134,32 @@ for entry in entries:
 	# [%name%](%assetfullpath%%assetfile%) | %size% | %lastmodified% | [view files](%pluginurl%%pluginnameurl%/) <br>
 	# %description% p_template
 
-	pa_template = pa_template.replace("%name%", entry)
-	pa_template = pa_template.replace("%assetfullpath%", assetfiles)
-	pa_template = pa_template.replace("%assetfile%", withdots + ".zip")
+	pa_template = pa_template.replace('%name%', entry)
+	pa_template = pa_template.replace('%assetfullpath%', assetfiles)
+	pa_template = pa_template.replace('%assetfile%', withdots + '.zip')
 	if assetsize != 'N/A':
-		pa_template = pa_template.replace("%size%", str(round(assetsize, 2)) + form)
+		pa_template = pa_template.replace('%size%', str(round(assetsize, 2)) + form)
 	else:
-		pa_template = pa_template.replace("%size%", assetsize)
-	pa_template = pa_template.replace("%lastmodified%", modif)
-	pa_template = pa_template.replace("%pluginurl%", pluginurl)
-	pa_template = pa_template.replace("%pluginnameurl%", forweb)
-	pa_template = pa_template.replace("%description%", description)
-	pa_template = pa_template.replace("%readme%", readme)
-	pa_template = pa_template.replace("%icon%", icon)
+		pa_template = pa_template.replace('%size%', assetsize)
+	pa_template = pa_template.replace('%lastmodified%', modif)
+	pa_template = pa_template.replace('%pluginurl%', pluginurl)
+	pa_template = pa_template.replace('%pluginnameurl%', forweb)
+	pa_template = pa_template.replace('%description%', description)
+	pa_template = pa_template.replace('%readme%', readme)
+	pa_template = pa_template.replace('%icon%', icon)
 	
 	
-	with open(indexfile, "a") as file1:
+	with open(indexfile, 'a') as file1:
 		file1.writelines(pa_template)
 	
-	print(entry + " WRITTEN")
+	print(entry + ' WRITTEN')
 	
 # deleting zip from workflow
 files = os.listdir()
 for file in files:
-	if file[len (file)  -3:] == "zip":
+	if file[len (file)  -3:] == 'zip':
 		os.remove(file)
-		print("\n" + file + " REMOVED")
+		print('\n' + file + ' REMOVED')
 
 if os.path.isdir('versioning'):
 	shutil.rmtree('versioning')
